939bc4b10e1b7243dc7df3c89007d4db627bcf08
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-01-28 23:09:02 +0100

    A new package manager has born (kinda)

--- lua/manpack/init.lua
+++ lua/manpack/init.lua
@@ -1,30 +1,73 @@
 local M = {}
-
-M.manpackpath = vim.fs.joinpath(vim.fn.stdpath("data"), "manpack")
+---@class ManpackPluginSpec
+---@field config function
+---@field branch string
+---@field event string|string[]
+---@field filetype string|string[]
 
 ---@param plugin_name string
-function M.plug(plugin_name)
+---@param spec? ManpackPluginSpec
+function M.load(plugin_name, spec)
     local plugin = "https://github.com/" .. plugin_name
-    local plugin_dir = vim.fs.joinpath(M.manpackpath, vim.fs.basename(plugin_name))
+    local manpackpath = vim.fn.stdpath("data") .. "/manpack"
+    local plugin_dir = manpackpath .. "/" .. vim.fs.basename(plugin_name)
 
     if not vim.uv.fs_stat(plugin_dir) then
-        vim.fn.system({"git", "clone", "--depth=1", plugin, plugin_dir})
+        local cmd = { "git", "clone", "--depth=1" }
+        if spec and spec.branch then
+            table.insert(cmd, "--branch=" .. spec.branch)
+        end
+
+        table.insert(cmd, plugin)
+        table.insert(cmd, plugin_dir)
+
+        local result = vim.fn.system(cmd)
+        if vim.v.shell_error ~= 0 then
+            print("Error al clonar el repositorio: " .. result)
+        else
+            print("Repositorio clonado exitosamente.")
+        end
     end
+
 
     vim.opt.runtimepath:append(plugin_dir)
 
-    local doc_dir = vim.fs.joinpath(plugin_dir, "doc")
+    local doc_dir = plugin_dir .. "/doc"
     if vim.uv.fs_stat(doc_dir) then
-        vim.schedule(function()
-            vim.cmd("helptags " .. doc_dir)
-        end)
+        vim.cmd.helptags(doc_dir)
+    end
+
+    if not spec then
+        return
+    end
+
+    if spec.config then
+        if spec.filetype then
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = type(spec.filetype) == "string" and { spec.filetype } or spec.filetype,
+                once = true,
+                group = vim.api.nvim_create_augroup(vim.fs.basename(plugin_name) .. "-start", { clear = true }),
+                desc = "Starts " .. vim.fs.basename(plugin_name),
+                callback = spec.config
+            })
+        elseif spec.event then
+            vim.api.nvim_create_autocmd(spec.event, {
+                once = true,
+                group = vim.api.nvim_create_augroup(vim.fs.basename(plugin_name) .. "-start", { clear = true }),
+                desc = "Starts " .. vim.fs.basename(plugin_name),
+                callback = spec.config
+            })
+        else
+            spec.config()
+        end
     end
 end
 
-
 function M.setup()
-    if not vim.uv.fs_stat(M.manpackpath) then
-        vim.uv.fs_mkdir(M.manpackpath, 511)
+    local manpackpath = vim.fn.stdpath("data") .. "/manpack"
+
+    if not vim.uv.fs_stat(manpackpath) then
+        vim.uv.fs_mkdir(manpackpath, 511)
     end
     _G.Manpack = M
 end
